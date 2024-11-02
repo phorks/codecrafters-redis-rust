f5d06baa2514a4ec2074b55e0edda2a57e36fd50
Pharaz Fadaei <pharaz.apps@gmail.com>
2024-11-03 03:28:04 +0330

    Implemented PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{fmt::Write, io::Read, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,16 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut input = String::new();
+                _stream.read_to_string(&mut input).unwrap();
+                match input.as_str() {
+                    "*1\r\n$4\r\nping\r\n" => {
+                        input.write_str("+PONG\r\n").unwrap();
+                    }
+                    _ => {}
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
