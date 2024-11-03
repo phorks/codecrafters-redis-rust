966ea793be9a39ea18809af09ab7432b4c9f43cf
Pharaz Fadaei <pharaz.apps@gmail.com>
2024-11-03 03:59:36 +0330

    Copied a solution using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
-#![allow(unused_imports)]
-use std::{
-    io::{BufRead, BufReader, BufWriter, Read, Write},
-    net::TcpListener,
-};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("accepted new connection");
-                let reader = BufReader::new(&stream);
-                let mut writer = BufWriter::new(&stream);
-                for line in reader.lines() {
-                    if let Ok(_line) = line {
-                        writer.write_all(b"+PONG\r\n").unwrap();
-                    } else {
-                        break;
-                    }
-                }
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (stream, addr) = listener.accept().await.unwrap();
+        println!("Accepted connection from {addr}");
+        tokio::spawn(async move {
+            match handle_connection(stream).await {
+                Ok(_) => println!("Success"),
+                Err(e) => println!("{e}"),
             }
-            Err(e) => {
-                println!("error: {}", e);
+            println!("Disconnected from {addr}");
+        });
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let (read, mut write) = stream.split();
+    let reader = BufReader::new(read);
+    let mut lines = reader.lines();
+    while let Some(line) = lines.next_line().await? {
+        println!("{:?}", line);
+        println!("Received {:?}", line);
+        match line.as_str() {
+            "ping" => {
+                write.write_all(b"+PONG\r\n").await?;
+                break;
             }
+            _ => continue,
         }
     }
+    return Ok(());
 }
